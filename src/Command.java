1937e8de22f14feba2113c76380ddcd5a50adce0
Senya <frog9615@gmail.com>
2022-02-03 19:10:46 +0300

    UPGRADE HOME

---
+++ src/Command.java
@@ -0,0 +1,3 @@
+public enum Command {
+    LIST, ADD, EXIT, NOT_RECOGNIZED
+}
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,71 @@
+import lombok.Getter;
+
 import java.util.Scanner;
 import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Getter
 public class Main {
+    static Command command;
+    static String foundCommand = "";
+    static String intEmail;
     static TreeSet<String> emailsList = new TreeSet<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean cheack = true;
-        String foundEmail = "";
         do {
-            System.out.println("Введите \"e-mail\": ");
-            String intEmail = sc.nextLine().trim();
-            Pattern pattern = Pattern.compile("");
+            System.out.println("Введите КОМАНДУ: ");
+            intEmail = sc.nextLine().trim();
+            Pattern pattern = Pattern.compile("^(ADD\\s)?(LIST)?(EXIT)?");
             Matcher matcher = pattern.matcher(intEmail);
             while (matcher.find()) {
-                foundEmail = intEmail.substring(matcher.start(), matcher.end());
-                System.out.println(foundEmail);
+                foundCommand = intEmail.substring(matcher.start(), matcher.end()).trim();
             }
-            if (foundEmail.length() > 0) {
-                emailsList.add(foundEmail);
-            } else {
-                System.out.println("Введён некроректный \"e-mail\"!");
+            setCommand();
+            switch (getCommand()) {
+                case LIST:
+                    if (emailsList.size() > 0) {
+                        for (String s :
+                                emailsList) {
+                            System.out.println(s);
+                        }
+                    } else System.out.println("Список пуст!");
+                    break;
+                case ADD:
+                    String[] strADD = intEmail.split("\\s", 2);
+                    if (strADD.length == 2 &&
+                            strADD[1].matches("^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+" +
+                                    "(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")) {
+                        emailsList.add(strADD[1]);
+                    } else {
+                        System.out.println("Введён некроректный \"e-mail\"!");
+                    }
+                    break;
+                case EXIT:
+                    System.out.println("\tПрограма завершена...");
+                    cheack = false;
+                    break;
+                default:
+                    System.out.println("\tКоманда не распознана!");
+                    break;
             }
+        } while(cheack);
+    }
 
-        } while (cheack);
+    static void setCommand() {
+        if (intEmail.matches("^LIST")) {
+            command = Command.LIST;
+        } else if (foundCommand.matches("^ADD")) {
+            command = Command.ADD;
+        } else if (intEmail.matches("^EXIT")) {
+            command = Command.EXIT;
+        } else command = Command.NOT_RECOGNIZED;
+    }
+
+    static Command getCommand() {
+        return command;
     }
 }
+
